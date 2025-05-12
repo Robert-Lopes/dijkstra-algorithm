e04467df37256a3d548e1eb049cb21ef8a59a1be
Robert Lopes Terra <contatorobertlopes@gmail.com>
2025-05-12 11:53:11 -0300

    Adição da classe Grafo

--- source/router/dijkstra.py
+++ source/router/dijkstra.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, UploadFile
 from source.service.dijkstra import AlgorithmDijkstra 
 
 api = APIRouter()
-service = AlgorithmDijkstra()
 
 @api.post("/custo",
         summary="Calcula o custo de um caminho em um grafo")
 def calcula_custo(verticeInicial: str, grafo: UploadFile):
+    service = AlgorithmDijkstra()
     return service.encontrar_caminho(grafo)
--- source/service/dijkstra.py
+++ source/service/dijkstra.py
@@ -2,11 +2,11 @@ import re
 from fastapi import UploadFile
 from typing import Dict
 
-class AlgorithmDijkstra:
-    def __init__(self):
-        self.custo: int = 0
-        self.verticeInicial : str | None = None
-    
+
+class Grafo:
+    def __init__(self, grafo: dict = {}):
+       self.grafo = grafo  
+
     def validar_arquivo(self, arquivo: UploadFile):
         linhas = arquivo.file.read().decode('utf-8').split("\n")
         
@@ -20,6 +20,16 @@ class AlgorithmDijkstra:
             
             vertice_ponderados = lista_vertice[1:]
             print(vertice_ponderados)
+   
+    def add_edge(self, vertice1, vertice2, weight):
+       if vertice1 not in self.grafo:  
+           self.grafo[vertice1] = {}  
+       self.grafo[vertice1][vertice2] = weight  
+
+class AlgorithmDijkstra:
+    def __init__(self):
+        self.custo: int = 0
+        self.verticeInicial : str | None = None
 
     def encontrar_caminho(self, arquivo: UploadFile):
         self.validar_arquivo(arquivo)
