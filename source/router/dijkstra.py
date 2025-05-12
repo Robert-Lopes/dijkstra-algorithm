61a2b177c7061df93db9ca6395696470c0699b89
Robert Lopes Terra <contatorobertlopes@gmail.com>
2025-05-12 20:33:03 -0300

    Construção incial dos métodos para calcular caminhos

--- source/router/dijkstra.py
+++ source/router/dijkstra.py
@@ -7,5 +7,7 @@ api = APIRouter()
         summary="Calcula o custo de um caminho em um grafo")
 def calcula_custo(verticeInicial: str, arquivoGrafo: UploadFile):
     grafo_definido = Grafo(arquivo=arquivoGrafo)
+    
     service = AlgorithmDijkstra(grafo=grafo_definido)
+    
     return service.grafo.lista_adjacencia
--- source/service/dijkstra.py
+++ source/service/dijkstra.py
@@ -1,5 +1,5 @@
 import re
-from typing import Dict
+from typing import Dict, Any
 from fastapi import UploadFile
 from heapq import heapify, heappop, heappush
 
@@ -13,7 +13,7 @@ class Grafo:
 
     def __validar_arquivo(self, arquivo: UploadFile):
         '''
-            Método que recebe um arquivo .txt e percorre para criar o grafo  
+            Método que recebe um arquivo .txt e o percorre para criar o grafo  
         '''
         if arquivo is None: 
             raise FileNotFoundError("O arquivo não pode ser nulo")
@@ -32,5 +32,23 @@ class Grafo:
             self.lista_adjacencia[principal][vertice_ligado] = int(vertice[1:])  
 
 class AlgorithmDijkstra:
+    '''
+        Classe que recebe um grafo e possui calcula o menor caminho por meio do
+        algoritmo de Dijkstra
+    '''
     def __init__(self, grafo: Grafo):
         self.grafo: Grafo = grafo
+    
+    def obter_caminho_vertice(self, fonte: str, destino: str) -> Dict[str, Any]:
+        '''
+            Método que faz com que, por meio de um vertice fonte, encontra
+            o melhor caminho para um destino
+        '''
+        pass
+
+    def obter_todos_caminhos(self, fonte: str) -> Dict[str, Any]:
+        '''
+            Método que encontra os menores caminhos para todos os vertice
+            baseado em um único vertice fonte
+        '''
+        pass 
