cb65af9bf5d38af5ae8b0e77df9cf97a78f32945
Robert Lopes Terra <contatorobertlopes@gmail.com>
2025-05-12 20:16:50 -0300

    Aplica representação dos grafos em lista de adjacência

--- source/router/dijkstra.py
+++ source/router/dijkstra.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, UploadFile
-from source.service.dijkstra import AlgorithmDijkstra 
+from source.service.dijkstra import AlgorithmDijkstra, Grafo
 
 api = APIRouter()
 
 @api.post("/custo",
         summary="Calcula o custo de um caminho em um grafo")
-def calcula_custo(verticeInicial: str, grafo: UploadFile):
-    service = AlgorithmDijkstra()
-    return service.encontrar_caminho(grafo)
+def calcula_custo(verticeInicial: str, arquivoGrafo: UploadFile):
+    grafo_definido = Grafo(arquivo=arquivoGrafo)
+    service = AlgorithmDijkstra(grafo=grafo_definido)
+    return service.grafo.lista_adjacencia
--- source/service/dijkstra.py
+++ source/service/dijkstra.py
@@ -1,35 +1,36 @@
 import re
-from fastapi import UploadFile
 from typing import Dict
-
+from fastapi import UploadFile
+from heapq import heapify, heappop, heappush
 
 class Grafo:
-    def __init__(self, grafo: dict = {}):
-       self.grafo = grafo  
+    '''
+        Classe usada para representar um grafo por meio de uma lista de Adjacência
+    '''
+    def __init__(self, arquivo: UploadFile | None = None):
+        self.lista_adjacencia: Dict[str, Dict]= {} 
+        self.__validar_arquivo(arquivo=arquivo)
 
-    def validar_arquivo(self, arquivo: UploadFile):
+    def __validar_arquivo(self, arquivo: UploadFile):
+        '''
+            Método que recebe um arquivo .txt e percorre para criar o grafo  
+        '''
+        if arquivo is None: 
+            raise FileNotFoundError("O arquivo não pode ser nulo")
+        
         linhas = arquivo.file.read().decode('utf-8').split("\n")
         
         for linha in linhas:
             lista_vertice = re.split(pattern=r":\s*|,\s*", string=linha)
-
-            if isinstance(lista_vertice[0], str) and len(lista_vertice[0]) == 1: 
-                vertice_destaco = lista_vertice[0]
-            else: 
-                raise ValueError("O vertice destacado deve ser apenas uma letra")
-            
-            vertice_ponderados = lista_vertice[1:]
-            print(vertice_ponderados)
-   
-    def add_edge(self, vertice1, vertice2, weight):
-       if vertice1 not in self.grafo:  
-           self.grafo[vertice1] = {}  
-       self.grafo[vertice1][vertice2] = weight  
+            self.__add_edge(lista_vertice[0], lista_vertice[1:])   
+    
+    def __add_edge(self, principal, lista_vertice):
+        if principal not in self.lista_adjacencia:  
+           self.lista_adjacencia[principal] = {}
+        for vertice in lista_vertice:  
+            vertice_ligado = str(vertice[0])   
+            self.lista_adjacencia[principal][vertice_ligado] = int(vertice[1:])  
 
 class AlgorithmDijkstra:
-    def __init__(self):
-        self.custo: int = 0
-        self.verticeInicial : str | None = None
-
-    def encontrar_caminho(self, arquivo: UploadFile):
-        self.validar_arquivo(arquivo)
+    def __init__(self, grafo: Grafo):
+        self.grafo: Grafo = grafo
